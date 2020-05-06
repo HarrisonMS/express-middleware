9052621b9a06c3d9e6f366048e23636d3eb38b7e
Harrison <hmseaborn@gmail.com>
2020-05-06 11:30:38 -0700

    installed packages and get request to show server running

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require("./api/server.js");
+const port = process.env.PORT || 5010;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port}\n`);
+});
